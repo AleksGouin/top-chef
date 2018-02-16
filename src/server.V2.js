1954cbfcc40e41050cab71e168f46a405c6a19cc
AleksGouin <ilikemyteachers@gmail.com>
2018-02-16 17:38:11 +0100

    feat(restaurants): fetch list of restaurants on Michelin.com

---
+++ src/server.V2.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var fs = require('fs');
+var request = require ('request');
+var cheerio = require ('cheerio');
+var app = express();
+
+var resultat = [];
+var i=1;
+while (i<36)
+{
+	var page = i.toString();
+	url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-';
+	var result = url.concat(i);
+	request(result, function (error, response, html) {
+		if (!error) {
+			var $ = cheerio.load(html);
+			
+			var title;
+			
+			$('.poi_card-display-title').filter(function () {
+				var titre = $(this);
+				title = titre.text().trim();
+				var json = {title: ""};
+				json.title = title;
+				resultat.push(json);
+			})
+		}
+		fs.writeFile('output.json', JSON.stringify(resultat, null, 4), function(err){
+			})
+	})
+	i++;
+}
+console.log('File successfully written! - Check your projet directory for the output !');
