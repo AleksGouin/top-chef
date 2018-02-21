b2ccfd0884426380e9995b80008e7807f0224010
AleksGouin <ilikemyteachers@gmail.com>
2018-02-21 19:52:52 +0100

    ajout promises

--- src/server.V2.js
+++ src/server.V2.js
@@ -3,9 +3,15 @@ var fs = require('fs');
 var request = require ('request');
 var cheerio = require ('cheerio');
 var app = express();
+var $ = require('jquery');
+
+
+var restos = [];
+
 
 var resultat = [];
 var i=1;
+
 while (i<36)
 {
 	var page = i.toString();
@@ -17,17 +23,24 @@ while (i<36)
 			
 			var title;
 			
-			$('.poi_card-display-title').filter(function () {
+			$('.poi_card-display-title').each(function () {
 				var titre = $(this);
-				title = titre.text().trim();
+				title = titre.text();
 				var json = {title: ""};
 				json.title = title;
 				resultat.push(json);
+				restos.push(title)
 			})
+			console.log(title);
 		}
 		fs.writeFile('output.json', JSON.stringify(resultat, null, 4), function(err){
 			})
 	})
 	i++;
 }
+console.log(resultat);
 console.log('File successfully written! - Check your projet directory for the output !');
+
+
+
+
