9f338afa2d6a6282850f994c7830618068ffafa1
AleksGouin <ilikemyteachers@gmail.com>
2018-03-04 16:40:20 +0100

    ajout de commentaires + toutes les fonctionnalitées pour récupérer un deal

--- src/server.js
+++ src/server.js
@@ -7,9 +7,11 @@ var Promise = require('promise');
 var https = require('https');
 
 
-var restos = [];
+var restos = []; // On définit les variables globales qui vont nous être uties
+var ids = [];
+var promos = [];
 
-function scrapping(){
+function scrapping(){ //On scrappe le site michelin
 	return new Promise (function(resolve, reject) {
 		var resultat = [];
 		var i=1;
@@ -44,17 +46,65 @@ function scrapping(){
 	})
 }
 
-function listRestos(){
-	console.log(restos[0])
+function listRestos(){ //on affiche les restaurants que l'on a scrappé
+	
+	var val;
+	for (var i in restos) { // je ne comprends pas pourquoi des fois ça marche et des fois non
+		val = restos[i];
+		console.log(val);
+	} 
+}
+
+function recupId(restaurants) { //on récupère les id des restaurants que l'on a scrappé
+	
+	for(var i in restaurants) {
+		
+		var url = 'https://m.lafourchette.com/api/restaurant-prediction?name='; //on utilise l'api de la fourchette afin de trouver l'id du restaurant
+		var rechercher = url.concat(restaurants[i])
+		request.get(rechercher, (err, res, body) => {
+			if (!err) {
+			let json = JSON.parse(body); //le document que l'on récupère est un Json
+			ids.push(json[0].id);	
+			}
+		})
+	}
+}
+
+function recupPromo(listId, restaurants){
+	for (var i in restaurants) {
+		var promoResto = [];
+		
+		var url = 'https://www.lafourchette.com/restaurant/'
+		var ad1 = url.concat(restaurants[i]) //on peut entrer le nom du restaurant avec les espaces dans la barre de recherches
+		var adfinale = ad1.concat(listId[i]) // ainsi on a l'adresse totale
+		
+		request(result, function (error, response, html) {
+				if (!error) {
+					var $ = cheerio.load(html);
+					
+					var promo;
+					
+					$('.saleType-title').each(function () { //pour chaque resto on regarde toutes les promos possibles
+						var promo = $(this);
+						promo = promo.text();
+						promoResto.push(promo); // on ajoute la promo a la liste des promos de l'établissement
+						
+						
+					})
+					setTimeout(function() {return resolve();}, 0)
+				}
+				fs.writeFile('output.json', JSON.stringify(resultat, null, 4), function(err){
+					})
+			})
+		
+	}
+	
 }
 
 scrapping().then(listRestos)
 
- 
-request.get('https://m.lafourchette.com/api/restaurant-prediction?name=quinsou', (err, res, body) => {
-  let json = JSON.parse(body);
-  console.log(json[0].id)
-});
+
+
 
 
 
